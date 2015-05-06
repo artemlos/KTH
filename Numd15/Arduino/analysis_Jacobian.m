6c0fa65c519dd86e8c1889cfcdda6fc7e8485ebf
Artem Los <artem@artemlos.net>
2015-05-06 21:46:09 +0200

    changing model

--- Numd15/Arduino/analysis_Jacobian.m
+++ Numd15/Arduino/analysis_Jacobian.m
@@ -6,14 +6,17 @@ hold on
 
 
 % Jacobian matrix:
-J = @(t,k,l) [exp(-k*t) -l*t.*exp(-k*t)]
+J = @(t,k,l) [ones(length(t),1) -t]
 1
 % Functions
-f = @(t,k,l) l*exp(-k*t);
+f = @(t,k,l) -k*t*+l;
 
 % I guess the solution to be close to (0,0)
-x=[5 5]',iter=0, dxnorm=1;
+%x=[5 5]';
+iter=0, dxnorm=1;
 
+temp = log(temp);
+x= [1 1]'
 while dxnorm>0.5e-4 & iter < 20
     fval = f(time, x(1),x(2))-temp;
     jval = J(time, x(1), x(2));
@@ -25,8 +28,8 @@ x,iter
 hold on
 %
 %plot(time,temp);
-time = -1000:0.1:1000;
-plot(time, f(time, x(1),x(2)))
+time = -10:0.01:10;
+%plot(time, f(time, x(1),x(2)))
 
 axis square
 title('Temperature vs. Time')
