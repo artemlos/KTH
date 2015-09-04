53b34c3dc762194b575809b556fbfb17ca726287
artem <artem@artemlos.net>
2015-09-04 11:32:11 +0200

    Add y as a vowel + develop other functions.

--- Progp/warmup.hs
+++ Progp/warmup.hs
@@ -1,5 +1,7 @@
 module F1 where
 
+import Data.Char
+
 -- Vad ska de andra funktionernas typsignaturer vara?
 {-
 slowest way
@@ -8,16 +10,36 @@ fib 0 = 0
 fib 1 = 1
 fib n = fib(n-1) + fib(n-2)
 -}
+-- the closed form of fib n is obviously faster then the conventional recurrence
 -- faster way https://wiki.haskell.org/The_Fibonacci_sequence#Using_Binet.27s_formula
 fib n = round $ phi ^ n / sq5
 	where
 	    sq5 = sqrt 5 
 	    phi = (1 + sq5) / 2
 
+rovarsprak :: String -> String
+rovarsprak "" = "" -- to stop the recursion
+rovarsprak s = if isConsonant( head s) 
+			   then [head s] ++ "o" ++[head s]++ rovarsprak( tail s)
+			   else [head s] ++ rovarsprak(tail s)
+
+isConsonant :: Char -> Bool
+isConsonant s = not('a' == s || 'e' == s || 'i' == s || 'o' == s || 'u' == s || 'y' == s)
+
+karpsravor :: String -> String
+karpsravor "" = ""
+karpsravor s = if isConsonant( head s) 
+			   then [head s]++karpsravor( drop 3 s)
+			   else [head s] ++ karpsravor(tail s)
+
+--medellangd :: String -> Double
+wordCount = 0
+charCount = 0
+--medellangd s = map(getCount)s
+
+getCount s = if isAlpha (head s)
+			 then 1
+			 else 0
 
 
-rovarsprak s = s
-karpsravor s = s
-medellangd s = 1.0
 skyffla s = s
-
